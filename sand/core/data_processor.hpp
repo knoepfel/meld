54616da1aaad81979a4c1e8c6fbdd2e89715fb1c
Kyle Knoepfel <knoepfel@fnal.gov>
2022-03-29 16:04:34 -0500

    Separate data_processor class.

---
+++ sand/core/data_processor.hpp
@@ -0,0 +1,44 @@
+#ifndef sand_core_data_processor_h
+#define sand_core_data_processor_h
+
+#include "sand/core/task_scheduler.hpp"
+
+#include <iostream>
+
+namespace sand {
+  template <typename Source>
+  class data_processor {
+  public:
+    explicit data_processor(std::size_t const n) : source_{n} {}
+
+    void
+    run_to_completion()
+    {
+      scheduler_.append_task([this] { next(); });
+      scheduler_.run();
+    }
+
+  private:
+    void
+    process(node& data)
+    {
+      std::cout << "Processing data "
+                << "(" << data.id() << ")\n";
+    }
+
+    void
+    next()
+    {
+      if (auto data = source_.next()) {
+        scheduler_.append_task([d = data, this] { process(*d); });
+        scheduler_.append_task([this] { next(); });
+      }
+    }
+
+    Source source_;
+    task_scheduler scheduler_{};
+  };
+
+}
+
+#endif
--- sand/run_sand.cpp
+++ sand/run_sand.cpp
@@ -1,44 +1,15 @@
 #include "sand/run_sand.hpp"
+#include "sand/core/data_processor.hpp"
 #include "sand/core/source.hpp"
-#include "sand/core/task_scheduler.hpp"
 
-#include <functional>
 #include <iostream>
 
 namespace sand {
   void
-  process(node& data)
-  {
-    std::cout << "Processing data "
-              << "(" << data.id() << ")\n";
-  }
-
-  struct DataProcessor {
-    explicit DataProcessor(std::size_t const n, task_scheduler& scheduler) :
-      source_{n}, scheduler_{scheduler}
-    {
-    }
-
-    void
-    execute()
-    {
-      if (auto data = source_.next()) {
-        scheduler_.append_task([d = data] { process(*d); });
-        scheduler_.append_task([this] { execute(); });
-      }
-    }
-
-    source source_;
-    task_scheduler& scheduler_;
-  };
-
-  void
   run(std::size_t const n)
   {
     std::cout << "Running sand\n";
-    task_scheduler scheduler{};
-    DataProcessor processor{n, scheduler};
-    scheduler.append_task([&processor] { processor.execute(); });
-    scheduler.run();
+    data_processor<source> processor{n};
+    processor.run_to_completion();
   }
 }
