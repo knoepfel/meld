92fc72a9e0490a5c548a96b67efe3fbd2219bd96
Kyle Knoepfel <knoepfel@fnal.gov>
2022-04-04 13:24:52 -0500

    Clang-format only.

--- sand/core/data_processor.hpp
+++ sand/core/data_processor.hpp
@@ -15,8 +15,7 @@ namespace sand {
     explicit data_processor(std::size_t const n,
                             std::unique_ptr<source_worker> sworker,
                             std::unique_ptr<module_worker> mworker) :
-      source_{std::move(sworker)},
-      worker_{std::move(mworker)}
+      source_{std::move(sworker)}, worker_{std::move(mworker)}
     {
     }
 
