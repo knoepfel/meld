d7a9f40c7a3a46f3ee6456361dae18aed68b60f5
Kyle Knoepfel <knoepfel@fnal.gov>
2022-04-04 13:12:57 -0500

    Support plugin lookup via environment variable.

--- sand/core/load_module.cpp
+++ sand/core/load_module.cpp
@@ -2,9 +2,13 @@
 #include "sand/core/module_worker.hpp"
 #include "sand/core/source_worker.hpp"
 
+#include "boost/algorithm/string.hpp"
 #include "boost/dll/import.hpp"
 
 #include <functional>
+#include <string>
+
+using namespace std::string_literals;
 
 namespace sand {
 
@@ -15,16 +19,28 @@ namespace sand {
     std::function<source_creator_t> create_source;
 
     template <typename creator_t>
-    auto
+    std::function<creator_t>
     plugin_loader(std::string const& spec, std::string const& symbol_name)
     {
-      // FIXME: Remove hard-coding and choose policy for searching for plugins.
-      std::filesystem::path shared_library_path("/Users/kyleknoepfel/work/build-sand/test");
-      shared_library_path /= spec;
+      char const* plugin_path_ptr = std::getenv("SAND_PLUGIN_PATH");
+      if (!plugin_path_ptr)
+        throw std::runtime_error("SAND_PLUGIN_PATH has not been set.");
 
-      using namespace boost::dll;
-      return import_alias<creator_t>(
-        shared_library_path, symbol_name, load_mode::append_decorations);
+      using namespace boost;
+      std::vector<std::string> subdirs;
+      split(subdirs, plugin_path_ptr, is_any_of(":"));
+
+      // FIXME: Need to test to ensure that first match wins.
+      for (auto const& subdir : subdirs) {
+        std::filesystem::path shared_library_path{subdir};
+        shared_library_path /= "lib" + spec + ".so";
+        if (exists(shared_library_path)) {
+          return dll::import_alias<creator_t>(shared_library_path, symbol_name);
+        }
+      }
+
+      throw std::runtime_error("Could not locate library with specification '"s + spec +
+                               "' in any directories on SAND_PLUGIN_PATH."s);
     }
   }
 
